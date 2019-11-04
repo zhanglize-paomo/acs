d7df3af8ab38b9474ff885a100419ed69072da87
zhanglize <Z18434395962@163.com>
2019-11-04 14:38:04 +0800

    搭建环境

---
+++ src/main/java/com/example/asc/AscApplication.java
@@ -0,0 +1,18 @@
+package com.example.asc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(AscApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscApplication.class, args);
+		logger.info("项目启动成功");
+	}
+
+}
